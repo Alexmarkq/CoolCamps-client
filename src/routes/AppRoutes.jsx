daa22fbc9b609dfbf96bdcdd318d05bafa8c6b99
Juan <jpcarta16@gmail.com>
2022-12-13 19:53:01 +0100

    last changes final MVp

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import RentDetailsPage from "../pages/RentDetailsPage/RentDetailsPage"
 import RentListPage from "../pages/RentListPage/RentListPage"
 import HomePage from "../pages/HomePage/HomePage"
-import NewRentPage from "../pages/NewRentPage/NewRentPage"
-import SignupPage from "../pages/SignupPage/SignupPage"
-import LoginPage from "../pages/LoginPage/LoginPage"
 import ProfilePage from "../pages/ProfilePage/ProfilePage"
 import PrivateRoute from "./PrivateRoute"
-import NewReviewPage from "../pages/NewRentPage/NewRentPage"
 
 
 
@@ -16,16 +12,10 @@ const AppRoutes = () => {
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/lista" element={<RentListPage />} />
-            <Route path="/crear" element={<NewRentPage />} />
             <Route path="/detalles/:rent_id" element={<RentDetailsPage />} />
-            <Route path="/iniciar-sesion" element={<LoginPage />} />
-            <Route path="/cerrar-sesion" element={<p>Cerrar Sesión</p>} />
-            <Route path="/registro" element={<SignupPage />} />
 
             <Route element={<PrivateRoute />}>
                 <Route path="/perfil" element={<ProfilePage />} />
-                <Route path="/comentario/crear/:rent_id" element={<NewReviewPage />} />
-                <Route path="/comentario/detalles/:rent_id" element={<RentDetailsPage />} />
             </Route>
 
             <Route path="/*" element={<h1>404</h1>} />
