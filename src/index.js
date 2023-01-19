ff56e1da196e9d53322f4da1a9b7bb266c89c519
Alex Marquez <alexis.marquez@icloud.com>
2023-01-19 11:51:36 +0100

    small changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
 import { BrowserRouter as Router } from 'react-router-dom'
 import { RentProviderWrapper } from './contexts/rent.context'
 import { AuthProviderWrapper } from './contexts/auth.context'
